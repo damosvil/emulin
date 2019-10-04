734e02184941dad58036163d4765ac2a3f61002f
Daniel Mosquera <blue@zenitsea.com>
2019-10-04 12:25:10 +0200

    Added error reporting when POSIX locale is not isntalled in the system

--- src/LIN.cpp
+++ src/LIN.cpp
@@ -11,10 +11,12 @@
 #include <locale.h>
 #include <gtk/gtk.h>
 #include <VentanaInicio.h>
+#include <tools.h>
 
 
 using namespace std;
 using namespace ui;
+using namespace tools;
 
 
 int main(int argc, char *argv[])
@@ -24,17 +26,23 @@ int main(int argc, char *argv[])
 
 	// Initialize GTK
 	gtk_init(&argc, &argv);
-	builder = gtk_builder_new();
-	gtk_builder_add_from_file(builder, "src/ui/Emulin.glade", &error);
 
-	// Change locale because GTK changes it to the system one
+	// Change locale because GTK changes it to the system one. Needed for floating point parsing.
 	if (setlocale(LC_ALL, "POSIX") != NULL)
 	{
+		// Start the user interface
+		builder = gtk_builder_new();
+		gtk_builder_add_from_file(builder, "src/ui/Emulin.glade", &error);
+
 		// Instantiate VentanaInicio
 		VentanaInicio v(builder);
 
 		// Run GTK's main loop
 		gtk_main();
+	}
+	else
+	{
+		ShowErrorMessageBox(NULL, "Emulin cannot start because for any reason POSIX locale is not installed in this system.");
 	}
 
 	return 0;
